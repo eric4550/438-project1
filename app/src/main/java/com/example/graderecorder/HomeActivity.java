815556f5d42b83675dd783347a1a7003d519b2f5
Brian <bbaird@csumb.edu>
2020-09-15 15:02:14 -0700

    Main screen. Intents need fix

--- app/src/main/java/com/example/graderecorder/HomeActivity.java
+++ app/src/main/java/com/example/graderecorder/HomeActivity.java
@@ -1,5 +1,6 @@
 package com.example.graderecorder;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -9,12 +10,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class HomeActivity extends AppCompatActivity {
 
     private TextView tvUser;
     private User user;
+
+    Button course;
+    Button assignments;
+    Button returnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,5 +34,49 @@ public class HomeActivity extends AppCompatActivity {
 //            tvUser.setText("Welcome ");
 //        }
 
+//        course = findViewById(R.id.coursesBtn);
+//        assignments = findViewById(R.id.assignmentsBtn);
+//        returnLogin = findViewById(R.id.btnLogin);
+//
+//
+//        course.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+////                openCourse();
+//            }
+//        });
+//
+//        assignments.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+////                openAssignments();
+//            }
+//        });
+//
+//        returnLogin.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+////                returnToLogin();
+//            }
+//        });
+//
+//    }
+//    public void openCourse(){
+//        Intent intent = new Intent (this, RegisterActivity.class);
+//        startActivity(intent);
+//    }
+//
+//    public void openAssignments(){
+//// Should go to assignment activity. Not main.
+////        Intent intent = new Intent (this, AssignmentActivity.class);
+//
+//        Intent intent = new Intent (this, RegisterActivity.class);
+//        startActivity(intent);
+//    }
+//
+//    public void returnToLogin(){
+//        Intent intent = new Intent (this, MainActivity.class);
+//        startActivity(intent);
     }
+
 }
