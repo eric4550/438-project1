588cbd08f8a3557009e01eae3c0b41e44c720d62
Brian <bbaird@csumb.edu>
2020-09-15 16:13:55 -0700

    Fixed duplicate error

--- app/src/main/java/com/example/graderecorder/HomeActivity.java
+++ app/src/main/java/com/example/graderecorder/HomeActivity.java
@@ -11,17 +11,13 @@ import android.widget.TextView;
 public class HomeActivity extends AppCompatActivity {
     private TextView tvUser;
     private User user;
-<<<<<<< HEAD
-    private Button course;
-    private Button assignments;
-    private Button returnLogin;
-=======
 
     private Button course;
     private Button assignments;
     private Button returnLogin;
 
->>>>>>> 3aecd40672161d79fde730b733a457096be8b781
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,59 +28,33 @@ public class HomeActivity extends AppCompatActivity {
 //        if(user != null) {
 //            tvUser.setText("Welcome ");
 //        }
-<<<<<<< HEAD
-        course = findViewById(R.id.coursesBtn);
-        assignments = findViewById(R.id.assignmentsBtn);
-        returnLogin = findViewById(R.id.returnLoginBtn);
-=======
 
         course = findViewById(R.id.coursesBtn);
         assignments = findViewById(R.id.assignmentsBtn);
         returnLogin = findViewById(R.id.returnLoginBtn);
 
 
->>>>>>> 3aecd40672161d79fde730b733a457096be8b781
         course.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 openCourse();
             }
         });
-<<<<<<< HEAD
-=======
-
->>>>>>> 3aecd40672161d79fde730b733a457096be8b781
         assignments.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 openAssignments();
             }
         });
-<<<<<<< HEAD
-=======
-
->>>>>>> 3aecd40672161d79fde730b733a457096be8b781
         returnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 returnToLogin();
             }
         });
-<<<<<<< HEAD
     }
-    public void openCourse(){
-        Intent intent = new Intent ( HomeActivity.this, Courses.class);
-        startActivity(intent);
-    }
-    public void openAssignments(){
-// Should go to assignment activity. Not main.
-//        Intent intent = new Intent (this, AssignmentActivity.class);
-        Intent intent = new Intent (HomeActivity.this, RegisterActivity.class);
-        startActivity(intent);
-    }
-=======
 
-    }
+
     public void openCourse(){
         Intent intent = new Intent ( HomeActivity.this, RegisterActivity.class);
         startActivity(intent);
@@ -98,7 +68,6 @@ public class HomeActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
->>>>>>> 3aecd40672161d79fde730b733a457096be8b781
     public void returnToLogin(){
         Intent intent = new Intent (HomeActivity.this, MainActivity.class);
         startActivity(intent);
